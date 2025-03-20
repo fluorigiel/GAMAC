8fa43080b58b87b4e73394be3426f8fa2966af62
Adrien-Belleau <belleauadri1@gmail.com>
2025-03-20 14:16:17 +0100

    FixedTriggerDetect

--- Unity/Castle_Kid/Assets/_Scripts/Player/Trigger/CustomTrigger.cs
+++ Unity/Castle_Kid/Assets/_Scripts/Player/Trigger/CustomTrigger.cs
@@ -5,17 +5,28 @@ namespace _Scripts.Player.Trigger
     public class CustomTrigger : MonoBehaviour
     {
 
+        private string GroundLayer = "Ground";
+        private string PlayerTag = "Player";
+
         public event System.Action<Collider2D> EnteredTrigger;
         public event System.Action<Collider2D> ExitedTrigger;
 
         private void OnTriggerEnter2D(Collider2D item)
         {
-            EnteredTrigger?.Invoke(item);
+            //EnteredTrigger.Invoke(item);
+            if (item.tag == PlayerTag || LayerMask.LayerToName(item.transform.parent.gameObject.layer) == GroundLayer) // long line to access the layer of the collider2D
+            {
+                EnteredTrigger.Invoke(item);
+            }
         }
 
         private void OnTriggerExit2D(Collider2D item)
         {
-            ExitedTrigger?.Invoke(item);
+            //ExitedTrigger.Invoke(item);
+            if (item.tag == PlayerTag || LayerMask.LayerToName(item.transform.parent.gameObject.layer) == GroundLayer) // long line to access the layer of the collider2D
+            {
+                ExitedTrigger.Invoke(item);
+            }
         }
     }
 }
